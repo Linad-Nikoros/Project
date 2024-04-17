a89b97d1e09bea97b3680e46dbc3b1e078f7056f
Linad-Nikoros <linad.nikoros@gmail.com>
2024-04-17 20:57:52 +0700

    first

---
+++ app.constants.ts
@@ -0,0 +1,6 @@
+export const APP_WRITE_ID = '660be278754539c84609'
+export const DB_ID = 'data-base-crm-system'
+export const COLLECTION_DEALS = 'deals'
+export const COLLECTION_CUSTOMERS = 'customers'
+export const COLLECTION_COMMENTS = 'comments'
+export const STORAGE_ID = 'storage'
---
+++ components/layout/menu.data.ts
@@ -0,0 +1,43 @@
+export interface IMenuItem {
+	name: string
+	url: string
+	icon: string
+}
+
+export const MENU_DATA: IMenuItem[] = [
+	{
+		icon: 'radix-icons:dashboard',
+		name: 'Домашняя',
+		url: '/',
+	},
+	{
+		name: 'Продукты',
+		icon: 'ep:goods',
+		url: '/products',
+	},
+	{
+		name: 'Платежи',
+		icon: 'ph:contactless-payment',
+		url: '/payments',
+	},
+	{
+		name: 'Заказы',
+		icon: 'fluent:receipt-28-regular',
+		url: '/orders',
+	},
+	{
+		name: 'Клиенты',
+		icon: 'mingcute:group-line',
+		url: '/customers',
+	},
+	{
+		name: 'Отзывы',
+		icon: 'fluent:person-feedback-48-regular',
+		url: '/feedback',
+	},
+	{
+		name: 'Настройки',
+		icon: 'radix-icons:gear',
+		url: '/settings',
+	},
+]
